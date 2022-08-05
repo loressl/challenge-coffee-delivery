f9a17a43d9854a933739835ee56c471524186d7f
loressl <loryssl@hotmail.com>
2022-08-04 22:16:24 -0300

    chore: Adiciona location e botão do cart

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,20 +2,21 @@ import { HeaderContainer } from "./styles";
 import logoCoffee from '../../assets/coffee-delivery-logo.svg'
 import { NavLink } from "react-router-dom";
 import { MapPin, ShoppingCart } from "phosphor-react";
+import { Button } from "../Button";
 
-export function Header(){
-    return(
+export function Header() {
+    return (
         <HeaderContainer>
-            <img src={logoCoffee} alt=""/>
-            <nav>
-                <NavLink to="/" title="Home">
-                    <MapPin size={24}/>
-                    Salvador
-                </NavLink>
-                <NavLink to="/checkout" title="Checkout">
+            <img src={logoCoffee} alt="" />
+            <div>
+                <div className="location">
+                    <MapPin size={24} weight="fill" />
+                    Salvador, BA
+                </div>
+                <Button>
                     <ShoppingCart size={24} />
-                </NavLink>
-            </nav>
+                </Button>
+            </div>
         </HeaderContainer>
     )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,14 +1,36 @@
 import styled from "styled-components";
 
-export const HeaderContainer = styled.header`
+export const HeaderContainer = styled.header.attrs(props => ({
+    className: props.className
+}))`
     display: flex;
     width: 100%;
-    height: 6.5rem;
     align-items: center;
     justify-content: space-between;
     padding: 2rem 10rem;
 
-    nav {
+    div {
+        display: flex;
+        gap: 0.75rem;
+        align-items: center;
+
+        & .location {
+            display: flex;
+            padding: 0.5rem;
+            gap: 0.25rem;
+            border-radius: 6px;
+            background: ${props => props.theme["purple-light"]};
+            font-size: 14px;
+            line-height: 1.3;
+            flex: 1;
+            color: ${props => props.theme["purple-dark"]};
+
+            svg {
+                color: ${props => props.theme["purple"]};
+            }
+        }
+    }
+    /* nav {
         display: flex;
         gap: 0.75rem;
         
@@ -16,5 +38,5 @@ export const HeaderContainer = styled.header`
             display: flex;
             gap: 0.25rem;
         }
-    }
+    } */
 `
