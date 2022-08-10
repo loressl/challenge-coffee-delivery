cedd7f09eb66ec8063961eeb3281cada0eaba5d7
loressl <loryssl@hotmail.com>
2022-08-09 22:26:35 -0300

    chore: Adicionada lib para criar server e atualizada pasta das imagens dos cafés

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,30 @@
+import { useEffect, useState } from "react";
 import { Intro } from "./components/Intro";
 import { HomeContainer } from "./styles";
 
 export function Home() {
-    return(
+    const [teste, setTeste] = useState('')
+
+    useEffect(() => {
+        const loadCoffees = async () => {
+            await fetch('http://localhost:3000/coffees')
+                .then(response => response.json())
+                .then(data => setTeste(data[0].urlImage))
+        }
+
+        loadCoffees()
+
+    }, [])
+
+    console.log(teste)
+
+    return (
         <HomeContainer>
-            <Intro/>
+            <Intro />
             <div className="coffee-list-container">
                 <p>Nossos cafés</p>
                 <div className="coffee-list">
-
+                    {/* <img src={teste} alt="teste" /> */}
                 </div>
             </div>
         </HomeContainer>
