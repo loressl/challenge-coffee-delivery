ead375956a2eb4d3d11f1e230b9f9169d4d72b24
loressl <loryssl@hotmail.com>
2022-08-10 23:06:58 -0300

    chore: Adicionado componente do card da lista de café

--- src/pages/Home/components/CardCoffee/index.tsx
+++ src/pages/Home/components/CardCoffee/index.tsx
@@ -1,5 +1,28 @@
+import { Minus, Plus } from "phosphor-react";
+import { ButtonSelectContainer, CardCoffeeContainer, Price } from "./styles";
+import coffeeTeste from '/coffee/arabe.svg'
+
 export function CardCoffee(){
     return(
-        <></>
+        <CardCoffeeContainer>
+            <img src={coffeeTeste}  alt=""/>
+            <span className="tag">tradicional</span>
+            <p className="name-coffee">Expresso Tradicional</p>
+            <p className="description">O tradicional café feito com água quente e grãos moídos</p>
+            <Price>
+                <p className="value">R$<strong> 9,90</strong></p>
+                <div className="actions">
+                    <ButtonSelectContainer>
+                        <button>
+                            <Minus size={14}/>
+                        </button>
+                        <span>1</span>
+                        <button>
+                            <Plus size={14}/>
+                        </button>
+                    </ButtonSelectContainer>
+                </div>
+            </Price>
+        </CardCoffeeContainer>
     )
 }
--- src/pages/Home/components/CardCoffee/styles.ts
+++ src/pages/Home/components/CardCoffee/styles.ts
@@ -1,5 +1,121 @@
 import styled from "styled-components";
 
 export const CardCoffeeContainer = styled.div`
-    
-`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  max-width: 16rem;
+  width: 100%;
+  height: 19.375rem;
+  background: ${(props) => props.theme["base-card"]};
+  border-radius: 6px 36px;
+
+  img {
+    width: 120px;
+    height: 120px;
+    margin-top: -20px;
+    margin-bottom: 0.75rem;
+  }
+
+  & .tag {
+    display: flex;
+    padding: 4px 8px;
+    width: 5.063rem;
+    height: 1.313rem;
+    background: ${(props) => props.theme["yellow-light"]};
+    border-radius: 100px;
+    font-size: 10px;
+    font-weight: 700;
+    text-transform: uppercase;
+    color: ${(props) => props.theme["yellow-dark"]};
+  }
+
+  & .name-coffee {
+    margin-top: 1rem;
+    font-weight: 700;
+    font-family: "Baloo 2";
+    font-size: 20px;
+    line-height: 1.3;
+    color: ${(props) => props.theme["base-subtitle"]};
+  }
+
+  & .description {
+    margin-top: 0.5rem;
+    text-align: center;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 1.3;
+    color: ${(props) => props.theme["base-label"]};
+    padding: 0 20px 0 20px;
+  }
+`;
+
+export const Price = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0;
+  margin-top: 33px;
+  gap: 29px;
+
+  & .value {
+    font-size: 14px;
+    line-height: 1.3;
+    text-align: right;
+    color: ${(props) => props.theme["base-text"]};
+
+    strong {
+      font-family: "Baloo 2";
+      font-weight: 800;
+      font-size: 24px;
+      line-height: 1.3;
+    }
+  }
+
+  & .actions {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+`;
+
+export const ButtonSelectContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px;
+  gap: 4px;
+  width: 4.5rem;
+  height: 2.375rem;
+  background: ${(props) => props.theme["base-button"]};
+  border-radius: 6px;
+
+  span {
+    text-align: center;
+    color: ${(props) => props.theme["base-title"]};
+  }
+
+  button {
+    color: ${(props) => props.theme["purple-dark"]};
+    display: flex;
+    height: 100%;
+    align-items: center;
+    transition: all 0.3s ease-in-out;
+
+    &:disabled {
+      cursor: not-allowed;
+    }
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+    &:hover {
+      filter: brightness(0.95);
+    }
+  }
+`;
