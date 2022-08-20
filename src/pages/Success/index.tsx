c3e5944a7cc31501460d26e5f945c3391dcd9a04
loressl <loryssl@hotmail.com>
2022-08-19 22:21:32 -0300

    feat: adicionado tela de pedido confirmado

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,13 +1,48 @@
-import { SuccessContainer } from "./styles";
+import { InfoLocal, OrderInfo, SuccessContainer } from "./styles";
 import illustration from '../../assets/illustration.svg'
+import { Clock, CurrencyDollar, MapPin } from "phosphor-react";
+import { ItemWithIcon } from "../../components/ItemWithIcon";
 
-export function Success(){
-    return(
+export function Success() {
+    return (
         <SuccessContainer>
             <div className="confirm-ordered">
-                success
+                <p>Uhu! Pedido confirmado</p>
+                <span>Agora é só aguardar que logo o café chegará até você</span>
+                <OrderInfo>
+                    <ItemWithIcon
+                        icon={<MapPin weight="fill" />}
+                        backgroundIconColor="purple"
+                        text={
+                            <InfoLocal>
+                                <span>Entrega em <strong>Rua João Daniel Martinelli, 102</strong></span>
+                                <span>Farrapos - Porto Alegre, RS</span>
+                            </InfoLocal>
+                        }
+                    />
+                    <ItemWithIcon
+                        icon={<Clock weight="fill" />}
+                        backgroundIconColor="yellow"
+                        text={
+                            <InfoLocal>
+                                <span>Previsão de entrega</span>
+                                <span><strong>20 min - 30 min</strong></span>
+                            </InfoLocal>
+                        }
+                    />
+                    <ItemWithIcon
+                        icon={<CurrencyDollar weight="fill" />}
+                        backgroundIconColor="yellowDark"
+                        text={
+                            <InfoLocal>
+                                <span>Pagamento na entrega</span>
+                                <span><strong>Cartão de Crédito</strong></span>
+                            </InfoLocal>
+                        }
+                    />
+                </OrderInfo>
             </div>
-            <img src={illustration} alt="Ilustração"/>
+            <img src={illustration} alt="Ilustração" />
         </SuccessContainer>
     )
 }
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -7,15 +7,63 @@ export const SuccessContainer = styled.div`
   flex-direction: row;
   justify-content: center;
   flex-wrap: wrap;
+  gap: 6.375rem;
 
-  & .confirm-ordered{
+  & .confirm-ordered {
     display: flex;
     flex-direction: column;
+    width: 34.5rem;
+    margin-top: 5rem;
+
+    p {
+      font-family: "Baloo 2";
+      font-weight: 800;
+      font-size: 2rem;
+      line-height: 1.3;
+      color: ${(props) => props.theme["yellow-dark"]};
+      margin-bottom: 0.25rem;
+    }
+
+    span {
+      font-size: 20px;
+      color: ${(props) => props.theme["base-subtitle"]};
+    }
   }
 
   img {
     width: 30.75rem;
     height: 18.313rem;
-    margin-top: 4.75rem;
+    margin-top: 11.25rem;
+  }
+`;
+
+export const OrderInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  background: ${({ theme }) => theme.background};
+  align-items: flex-start;
+  padding: 2.5rem;
+  gap: 2rem;
+  border-radius: 6px 36px;
+  margin-top: 2.5rem;
+
+  &::before {
+    content: "";
+    position: absolute;
+    inset: -1px;
+    z-index: -1;
+    border-radius: 7px 37px 7px 37px;
+    background: linear-gradient(102.89deg, #dbac2c 2.61%, #8047f8 98.76%);
+  }
+`;
+
+export const InfoLocal = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: ${(props) => props.theme["base-text"]};
+
+  span {
+    font-size: 1rem !important;
   }
 `;
